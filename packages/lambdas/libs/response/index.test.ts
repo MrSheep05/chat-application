2a6e3a88519c756512458fe9926171d7f665b640
Filip Wachowski <fifiw2005@gmail.com>
2024-04-11 20:35:00 +0200

    Bad request test

--- packages/lambdas/libs/response/index.test.ts
+++ packages/lambdas/libs/response/index.test.ts
@@ -1,52 +1,54 @@
-import { createResponse } from '.';
+import { createResponse } from ".";
 
-describe('Given the creteResponse function', () => {
-    it('should return generic 200 response by default', () => {
-        const response = createResponse({});
+describe("Given the creteResponse function", () => {
+  it("should return generic 200 response by default", () => {
+    const response = createResponse({});
 
-        expect(response).toStrictEqual({
-            body: 'OK',
-            headers: { "Access-Control-Allow-Origin": "*" },
-            statusCode: 200
-        });
+    expect(response).toStrictEqual({
+      body: "OK",
+      headers: { "Access-Control-Allow-Origin": "*" },
+      statusCode: 200,
     });
+  });
 
-    test.each([
-        {
-            statusCode: 200,
-            expectedBody: 'OK'
+  test.each([
+    {
+      statusCode: 200,
+      expectedBody: "OK",
+    },
+    {
+      statusCode: 400,
+      expectedBody: "Bad Request",
+    },
+    {
+      statusCode: 500,
+      expectedBody: "Internal Server Error",
+    },
+  ])(
+    "should return the correct default body for statusCode $code",
+    ({ statusCode, expectedBody }) => {
+      const response = createResponse({ statusCode });
+
+      expect(response).toStrictEqual({
+        statusCode,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
         },
-        {
-            statusCode: 400,
-            expectedBody: 'Not Found'
-        },
-        {
-            statusCode: 500,
-            expectedBody: 'Internal Server Error'
-        }
-    ])('should return the correct default body for statusCode $code', ({ statusCode, expectedBody }) => {
-        const response = createResponse({ statusCode });
+        body: expectedBody,
+      });
+    }
+  );
 
-        expect(response).toStrictEqual({
-            statusCode,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: expectedBody
-        })
-
+  it("should allow to override the body with a custom message", () => {
+    const response = createResponse({
+      statusCode: 200,
+      message: "Everything went well",
     });
 
-    it('should allow to override the body with a custom message', () => {
-        const response = createResponse({
-            statusCode: 200,
-            message: 'Everything went well'
-        });
-
-        expect(response).toStrictEqual({
-            body: JSON.stringify({ message: 'Everything went well' }),
-            headers: { "Access-Control-Allow-Origin": "*" },
-            statusCode: 200
-        });
+    expect(response).toStrictEqual({
+      body: JSON.stringify({ message: "Everything went well" }),
+      headers: { "Access-Control-Allow-Origin": "*" },
+      statusCode: 200,
     });
+  });
 });
