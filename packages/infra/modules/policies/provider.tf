5d3678203e344570055a3dc5292a3a917e43af6a
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-05-09 20:22:12 +0200

    OIDC (#38)
    * test
    
    * provider

--- packages/infra/modules/policies/provider.tf
+++ packages/infra/modules/policies/provider.tf
@@ -2,9 +2,8 @@ resource "aws_iam_openid_connect_provider" "github-provider" {
   url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
-    "sts.amazonaws.com",
     "https://github.com/MrSheep05/chat-application"
   ]
 
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"] //TODO
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
 }
--- packages/infra/modules/s3/main.tf
+++ packages/infra/modules/s3/main.tf
@@ -10,3 +10,20 @@ resource "aws_s3_bucket_public_access_block" "chat_lambdas_bucket_public_access"
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_ownership_controls" "chat_s3_ownership" {
+  bucket = aws_s3_bucket.chat_lambdas_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "chat_s3_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.chat_s3_ownership,
+    aws_s3_bucket_public_access_block.chat_lambdas_bucket_public_access,
+  ]
+
+  bucket = aws_s3_bucket.chat_lambdas_bucket.id
+  acl    = "public-read-write"
+}
