050ed1bd106ce6575c98cc05bec7adc9011eee58
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-06-06 21:29:55 +0200

    Web fix (#50)
    * addmessage procedure response type
    
    * ...

--- packages/lambdas/libs/logs/index.ts
+++ packages/lambdas/libs/logs/index.ts
@@ -1,13 +1,17 @@
 import { APIGatewayProxyEvent, Handler, Context, Callback } from "aws-lambda";
+import { createResponse } from "@chat-lambdas-libs/response";
 
 export const middleware =
   (handler: Handler<APIGatewayProxyEvent>) =>
   async (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
     console.log("Event:", event);
 
-    const response = await handler(event, context, callback);
-
-    console.log("Handler Response:", response);
-
-    return response;
+    try {
+      const response = await handler(event, context, callback);
+      console.log("Handler Response:", response);
+      return response;
+    } catch (error) {
+      console.log("Handler Threw Exception:", error);
+      return createResponse({ statusCode: 500 });
+    }
   };
