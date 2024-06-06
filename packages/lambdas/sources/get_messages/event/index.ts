a065455b365623eaaeee7d2299bc32b71cc4ff98
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-06-06 21:14:40 +0200

    fix (#49)

--- packages/lambdas/sources/get_messages/event/index.ts
+++ packages/lambdas/sources/get_messages/event/index.ts
@@ -1,8 +1,13 @@
 import { GetDataFromEventFn } from "./types";
 
 export const getDataFromEvent: GetDataFromEventFn = (event) => {
-  const { connectionId } = event.requestContext;
-  if (!event.body || !connectionId) return;
-  const { oldestMessageId } = JSON.parse(event.body).payload;
-  return { oldestMessageId: oldestMessageId ?? null, connectionId };
+  try {
+    const { connectionId } = event.requestContext;
+    if (!event.body || !connectionId) return;
+    console.info("getDataFromEvent");
+    const { oldestMessageId } = JSON.parse(event.body).payload;
+    return { oldestMessageId: oldestMessageId ?? null, connectionId };
+  } catch (e) {
+    console.error(e);
+  }
 };
