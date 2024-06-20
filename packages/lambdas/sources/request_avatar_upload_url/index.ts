87bd1c9ccea4b1a2029249b21dff5c1b7e4f5922
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-06-20 21:17:56 +0200

    user (#57)

--- packages/lambdas/sources/request_avatar_upload_url/index.ts
+++ packages/lambdas/sources/request_avatar_upload_url/index.ts
@@ -1,8 +1,14 @@
 import { middleware } from "@chat-lambdas-libs/logs";
 import { APIGatewayProxyEvent, Handler } from "aws-lambda";
+import { getUserData } from "./database";
+import { createResponse } from "@chat-lambdas-libs/response";
 
 export const handler: Handler<APIGatewayProxyEvent> = middleware(
   async (event) => {
     console.info("Message", event.body);
+    const { connectionId } = event.requestContext;
+    if (!connectionId) return createResponse({ statusCode: 500 });
+    const userData = await getUserData(connectionId);
+    console.info("USERDATA ", userData);
   }
 );
