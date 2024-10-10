4ca4a693b10deaa17ea1fbbdf3b705eaf2a9b534
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-10-10 20:20:35 +0200

    user profile table (#74)

--- packages/lambdas/sources/on_avatar_upload/index.ts
+++ packages/lambdas/sources/on_avatar_upload/index.ts
@@ -1,8 +1,41 @@
 import { middleware } from "@chat-lambdas-libs/logs";
-import { S3ObjectCreatedNotificationEvent, Handler } from "aws-lambda";
+import { Handler, S3Event } from "aws-lambda";
 
-export const handler: Handler<S3ObjectCreatedNotificationEvent> = middleware(
-  async (event) => {
-    console.info("EVENT", JSON.stringify(event));
+/*
+{
+    "Records": [
+        {
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "tf-s3-lambda-20241003195716022400000001",
+                "bucket": {
+                    "name": "chat-avatars-0luhsnz0s2",
+                    "ownerIdentity": {
+                        "principalId": "A3RCUXGS87QXMI"
+                    },
+                    "arn": "arn:aws:s3:::chat-avatars-0luhsnz0s2"
+                },
+                "object": {
+                    "key": "users/4bc22ead-2581-11ef-a9eb-0afd7d52d28d/avatar.png",
+                    "size": 22937,
+                    "eTag": "d329718a334e6cfa4c4302077290270c",
+                    "sequencer": "00670811B00B526EC7"
+                }
+            }
+        }
+    ]
+}
+*/
+
+export const handler: Handler<S3Event> = middleware(async (event) => {
+  console.info("EVENT", JSON.stringify(event));
+  const [firstRecord] = event.Records;
+
+  if (!firstRecord) {
+    // return 400
   }
-);
+
+  const { key } = firstRecord.s3.object;
+
+  const [folder, uid] = key.split("/");
+});
---
+++ packages/mysql/changesets/13-user-profile.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+-- changeset liquibase:user-profile-table
+CREATE TABLE chat.user_profile (
+  `id` VARCHAR(36) UNIQUE PRIMARY KEY NOT NULL DEFAULT (UUID()),
+  `user_id` VARCHAR(36) NOT NULL UNIQUE,
+  `avatar_key` VARCHAR(255) NOT NULL DEFAULT ("users/default/avatar.png")
+);
+-- rollback DROP TABLE chat.user_profile
+
+-- changeset liquibase:add_foreign_key_user_id_to_user_profile
+ALTER TABLE `user_profile` ADD CONSTRAINT `fk__user_profile__user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+-- rollback ALTER TABLE message DROP FOREIGN KEY `fk__user_profile__user_id`;
+
+-- changeset liquibase:existing_users_profile
+INSERT INTO chat.user_profile (user_id) VALUES (SELECT id FROM user);
+-- rollback DELETE FROM chat.user_profile
