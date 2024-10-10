4744cfce06e69491e73bb63152fd7e61a33c648d
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-10-10 21:08:34 +0200

    register user procedure (#76)

---
+++ packages/mysql/changesets/14-user-profile-procedures.sql
@@ -0,0 +1,50 @@
+--liquibase formatted sql
+
+-- changeset liquibase:replace_procedure_register_user endDelimiter:// rollbackEndDelimiter://
+DROP PROCEDURE RegisterUser //
+
+CREATE PROCEDURE RegisterUser(IN input_username VARCHAR(20), IN input_password VARCHAR(44))
+BEGIN
+    IF input_username IS NULL OR input_username = ''
+    THEN
+        SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'Username is null or empty', MYSQL_ERRNO = 1001;
+    END IF;
+    
+    IF input_password IS NULL OR input_password = ''
+    THEN
+        SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'Password is null or empty', MYSQL_ERRNO = 1001;
+    END IF;
+    
+    IF EXISTS (SELECT username FROM chat.user WHERE username = input_username)
+    THEN
+        SIGNAL SQLSTATE '10001' SET MESSAGE_TEXT = 'User already exists', MYSQL_ERRNO = 1001;
+    ELSE
+        SET @id = UUID();
+        INSERT INTO chat.user (id,username, password) VALUES (@id,input_username, input_password);
+        SET @updated_rows = SELECT ROW_COUNT() 'updateCount';
+        INSERT INTO chat.user_profile (user_id) VALUES (@id);
+        SELECT @updated_rows;
+    END IF;
+END //
+
+-- rollback DROP PROCEDURE RegisterUser
+-- rollback CREATE PROCEDURE RegisterUser(IN input_username VARCHAR(20), IN input_password VARCHAR(44))
+-- rollback BEGIN
+-- rollback     IF input_username IS NULL OR input_username = ''
+-- rollback     THEN
+-- rollback         SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'Username is null or empty', MYSQL_ERRNO = 1001;
+-- rollback     END IF;
+-- rollback     
+-- rollback     IF input_password IS NULL OR input_password = ''
+-- rollback     THEN
+-- rollback         SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'Password is null or empty', MYSQL_ERRNO = 1001;
+-- rollback     END IF;
+-- rollback     
+-- rollback     IF EXISTS (SELECT username FROM chat.user WHERE username = input_username)
+-- rollback     THEN
+-- rollback         SIGNAL SQLSTATE '10001' SET MESSAGE_TEXT = 'User already exists', MYSQL_ERRNO = 1001;
+-- rollback     ELSE
+-- rollback         INSERT INTO chat.user (username, password) VALUES (input_username, input_password);
+-- rollback         SELECT ROW_COUNT() 'updateCount';
+-- rollback     END IF;
+-- rollback END //
