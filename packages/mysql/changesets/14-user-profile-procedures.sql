e45f225bcb4363b5af81b470cc43689ce9033f57
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-10-10 21:41:00 +0200

    User profile (#79)
    * update user avatar sp
    
    * update user avatar sp

--- packages/mysql/changesets/14-user-profile-procedures.sql
+++ packages/mysql/changesets/14-user-profile-procedures.sql
@@ -48,3 +48,21 @@ END //
 -- rollback         SELECT ROW_COUNT() 'updateCount';
 -- rollback     END IF;
 -- rollback END //
+
+-- changeset liquibase:add_procedure_update_profile_avatar endDelimiter://
+CREATE PROCEDURE UpdateUserProfileAvatar(IN input_user_id VARCHAR(36), IN input_avatar_key VARCHAR(255))
+BEGIN
+     IF input_user_id IS NULL
+    THEN
+        SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'User id is null or empty', MYSQL_ERRNO = 1001;
+    END IF;
+
+    IF input_avatar_key IS NULL OR input_avatar_key = ''
+    THEN
+        SIGNAL SQLSTATE '10000' SET MESSAGE_TEXT = 'Avatar key is null or empty', MYSQL_ERRNO = 1001;
+    END IF;
+
+    UPDATE chat.user_profile SET avatar_key = input_avatar_key WHERE user_id = input_user_id;
+    SELECT ROW_COUNT() 'updateCount';
+END //
+-- rollback DROP PROCEDURE UpdateUserProfileAvatar
