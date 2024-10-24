18a19eccefeadad0dc2fd089370b6b7862ef22e9
Filip Wachowski <fifiw2005@gmail.com>
2024-10-24 19:54:49 +0200

    onAvatarUpload lambda

--- packages/lambdas/sources/on_avatar_upload/index.ts
+++ packages/lambdas/sources/on_avatar_upload/index.ts
@@ -1,5 +1,7 @@
 import { middleware } from "@chat-lambdas-libs/logs";
+import { createResponse } from "@chat-lambdas-libs/response";
 import { Handler, S3Event } from "aws-lambda";
+import { updateUserProfileAvatar } from "./database";
 
 /*
 {
@@ -28,14 +30,28 @@ import { Handler, S3Event } from "aws-lambda";
 */
 
 export const handler: Handler<S3Event> = middleware(async (event) => {
-  console.info("EVENT", JSON.stringify(event));
+  console.info("Event:", JSON.stringify(event));
   const [firstRecord] = event.Records;
 
-  if (!firstRecord) {
-    // return 400
+  if (!firstRecord?.s3?.object) {
+    console.error("Received invalid event format");
+    return createResponse({
+      statusCode: 400,
+      message: "Invalid format",
+    });
   }
 
-  const { key } = firstRecord.s3.object;
-
+  const { key = "" } = firstRecord.s3.object;
   const [folder, uid] = key.split("/");
+
+  if (folder !== "users" || !uid) {
+    console.error("Invalid avatar location received:", { folder, key });
+    return createResponse({
+      statusCode: 400,
+      message: "Invalid avatar location",
+    });
+  }
+
+  const response = await updateUserProfileAvatar({ uid, avatarKey: key });
+  console.info("Response from query:", JSON.stringify(response));
 });
