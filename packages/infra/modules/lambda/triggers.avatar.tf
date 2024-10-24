97b05fb3118924ac86a580562cbda2402b67dd5e
Filip Wachowski <fifiw2005@gmail.com>
2024-10-24 21:55:13 +0200

    rename

--- packages/infra/modules/lambda/triggers.avatar.tf
+++ packages/infra/modules/lambda/triggers.avatar.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_notification" "bucket_notification_on_upload" {
   bucket = var.s3_avatar_bucket_name
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_functions["on_avatar_upload"].arn
+    lambda_function_arn = aws_lambda_function.lambda_functions["on_avatar_uploaded"].arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "uploads/"
   }
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_notification" "bucket_notification_on_upload" {
 resource "aws_lambda_permission" "allow_bucket_on_upload" {
   statement_id  = "AllowExecutionFromS3AvatarBucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_functions["on_avatar_upload"].arn
+  function_name = aws_lambda_function.lambda_functions["on_avatar_uploaded"].arn
   principal     = "s3.amazonaws.com"
   source_arn    = var.s3_avatar_bucket_arn
 }
