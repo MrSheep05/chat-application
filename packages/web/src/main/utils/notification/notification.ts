e5480ca9eb986667f7c2a8f09ffbbb8908261b8f
Filip <91011923+MrSheep05@users.noreply.github.com>
2025-02-06 21:07:05 +0100

    png ext (#105)

--- packages/web/src/main/utils/notification/notification.ts
+++ packages/web/src/main/utils/notification/notification.ts
@@ -1,12 +1,18 @@
 import { App, Notification } from 'electron';
 
 const showNotification = (app: App, args: string[]) => {
-  const [username, message] = args;
-  const messageToShow =
-    message.length >= 25 ? `${message.substring(0, 24)}...` : message;
-  new Notification({ title: username, body: messageToShow }).show();
+    const [username, message] = args;
+    const messageToShow =
+        message.length >= 25 ? `${message.substring(0, 24)}...` : message;
+    // new Notification({ title: username, body: messageToShow }).show();
 
-  const badgeCount = app.getBadgeCount();
-  app.setBadgeCount(badgeCount + 1);
+    new Notification({
+        title: username,
+        body: messageToShow,
+        icon: 'https://chat-avatars-0luhsnz0s2.s3.eu-west-2.amazonaws.com/users/7c02ac16-2581-11ef-a9eb-0afd7d52d28d/avatar.png',
+    }).show();
+
+    const badgeCount = app.getBadgeCount();
+    app.setBadgeCount(badgeCount + 1);
 };
 export default showNotification;
